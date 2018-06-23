3071cc87325ef6a56eec0f32f206da67239da539
金鸿 <jinhong.zjh@alibaba-inc.com>
2018-06-23 17:57:01 +0800

    feat: upgrade to react16

--- demo/MatrixDemo.jsx
+++ demo/MatrixDemo.jsx
@@ -39,6 +39,33 @@ const data2 = {
   ],
 };
 
+const data3 = {
+  data: [
+    { x: 0, y: 0, row: 1, col: 1, type: 'title', text: '订单编号' },
+    { x: 1, y: 0, row: 1, col: 1, type: 'normal', text: '600,000.00(CNY)' },
+    { x: 2, y: 0, row: 1, col: 1, type: 'title', text: '申请单号' },
+    { x: 3, y: 0, row: 1, col: 1, type: 'normal', text: '123123' },
+    { x: 4, y: 0, row: 1, col: 1, type: 'title', text: '报价单号' },
+    { x: 5, y: 0, row: 1, col: 3, type: 'normal', text: '1234' },
+    { x: 0, y: 1, row: 1, col: 1, type: 'title', text: '订单状态' },
+    { x: 1, y: 1, row: 1, col: 1, type: 'normal', text: '已生效' },
+    { x: 2, y: 1, row: 1, col: 1, type: 'title', text: '订单金额' },
+    { x: 3, y: 1, row: 1, col: 1, type: 'normal', text: '12654300' },
+    { x: 4, y: 1, row: 1, col: 1, type: 'title', text: '收款公司' },
+    { x: 5, y: 1, row: 1, col: 1, type: 'normal', text: '阿里巴巴' },
+    { x: 6, y: 1, row: 1, col: 1, type: 'title', text: '供应商' },
+    { x: 7, y: 1, row: 1, col: 1, type: 'normal', text: '扒拉扒拉童装' },
+    { x: 0, y: 2, row: 1, col: 1, type: 'title', text: '已付总金额' },
+    { x: 1, y: 2, row: 1, col: 1, type: 'normal', text: '600,000.00(CNY)' },
+    { x: 2, y: 2, row: 1, col: 1, type: 'title', text: '已收款总金额' },
+    { x: 3, y: 2, row: 1, col: 1, type: 'normal', text: '123123' },
+    { x: 4, y: 2, row: 1, col: 1, type: 'title', text: '开票信息' },
+    { x: 5, y: 2, row: 1, col: 3, type: 'link', text: '开票信息可点击' },
+    { x: 0, y: 3, row: 1, col: 1, type: 'title', text: '备注' },
+    { x: 1, y: 3, row: 1, col: 7, type: 'normal', text: '吧啦的哈佛哈开的奶茶啊我会成为啊' },
+  ],
+};
+
 class Demo extends React.Component {
   static customRender(cellData) {
     const style = {
@@ -66,8 +93,18 @@ class Demo extends React.Component {
 
   constructor(props) {
     super(props);
+
     this.state = {
+      matrix2Data: data2,
     };
+
+    this.changeData = this.changeData.bind(this);
+  }
+
+  changeData() {
+    this.setState({
+      matrix2Data: data3,
+    });
   }
 
   render() {
@@ -86,8 +123,9 @@ class Demo extends React.Component {
           width={810}
         />
         <h2>应用场景</h2>
+        <button onClick={this.changeData}>修改数据重新布局</button>
         <Matrix
-          data={data2}
+          data={this.state.matrix2Data}
           cellWidth={120}
           render={Demo.customRender}
         />
